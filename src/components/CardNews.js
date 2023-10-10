14479a45c856635028b9d0f6962c5f986d76b32c
Luis Mello <luis.mello@fatec.sp.gov.br>
2023-10-09 21:15:44 -0300

    estilização

--- src/components/CardNews.js
+++ src/components/CardNews.js
@@ -9,40 +9,80 @@ class Cardnews extends HTMLElement{
 
     }
 
-    build(){
+    build() {
         const componentRoot = document.createElement("div");
-
         componentRoot.setAttribute("class", "card");
+    
+        const cardLeft = document.createElement("div");
         cardLeft.setAttribute("class", "card__left");
-        cardRight.setAttribute("class", "card__right");
-
-        const cardLeft= document.createElement("div");
-
+    
         const autor = document.createElement("span");
-        autor.textContent="By "+ (this.getAttribute("autor") || "anonymous");
-
+        autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous");
+    
         const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url");
+    
         const newsContent = document.createElement("p");
-
+        newsContent.textContent = this.getAttribute("contet");
+    
         cardLeft.appendChild(autor);
         cardLeft.appendChild(linkTitle);
         cardLeft.appendChild(newsContent);
-
-        const cardRight= document.createElement("div");
-
+    
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card__right");
+    
         const newsImage = document.createElement("img");
+        newsImage.src = (this.getAttribute("photo") || "assets/photo.jpg");
+        newsImage.alt = "Foto da Noticia";
         cardRight.appendChild(newsImage);
-
+    
         componentRoot.appendChild(cardLeft);
         componentRoot.appendChild(cardRight);
-
-
-
-        return componentRoot
-    }
+    
+        return componentRoot;
+      }
 
     styles(){
+        const style = document.createElement("style");
+         style.textContent=`
 
+            .card{
+                width: 720px;
+                justify-content: space-between;
+                box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+                -webkit-box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+                -moz-box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+                display: flex;
+                flex-direction: row;
+            }
+            
+            .card__left{
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                padding-left: 10px;
+            }
+            
+            .card__left > a{
+                margin-top: 15px;
+                font-size: 25px;
+                color: black;
+                text-decoration: none;
+                font-weight: bold;
+            }
+            
+            .card__left > p{
+                color: rgb(70, 70, 70);
+            }
+            
+            .card__left > span{
+                font-weight:400;
+            }         
+         `
+
+        return style;
     }
 
 }
